567f51446a2bc9057152ed620a35b0c92cc8fb9e
kah5161 <kah5161@rit.edu>
2024-04-03 19:31:15 -0500

    added import back

--- src/display_eink.py
+++ src/display_eink.py
@@ -18,7 +18,7 @@ EPD_WIDTH = 800
 EPD_HEIGHT = 480
 
 import logging
-# from waveshare_epd import epd7in5_V2
+from waveshare_epd import epd7in5_V2
 import time
 from PIL import Image, ImageDraw, ImageFont
 import traceback
